5bef9545bfb10c608a276204ee420ac53760b8ac
jordangatewood28 <165770705+jordangatewood28@users.noreply.github.com>
2024-05-09 15:05:36 -0700

    Update javascript.js

--- a3/javascript.js
+++ a3/javascript.js
@@ -1,6 +1,8 @@
 function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
   upRight(pHeight, pColorEven, pColorOdd, pSymbol);
-  downRight(pHeight, pColorEven, pColorOdd, pSymbol);}
+  downRight(pHeight, pColorEven, pColorOdd, pSymbol);
+  upLeft(pHeight, pColorEven, pColorOdd, pSymbol);
+  downLeft(pHeight, pColorEven, pColorOdd, pSymbol);}
 
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol){
   var rLine ="";
@@ -42,6 +44,51 @@ function downRight(pHeight, pColorEven, pColorOdd, pSymbol){
     // console.log(rLine);
 
 }
+
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+  var lLine ="";
+  for (i=0;i<pHeight;i++){
+    lLine +="<p>";
+    //Create each line on the Rhombus
+    for(j=0;j<=i;j++){
+
+//Is the position even or odd so we change the color
+      if (j%2)
+//even
+        lLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+      else
+//odd
+        lLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+    }
+    lLine +="</p>";
+    // console.log(rLine);
+  }
+
+document.getElementById("upRight").innerHTML = rLine;
+}
+
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+  var lLine ="";
+  for (i=pHeight;i > 0;i--){
+    lLine +="<p>";
+    //Create each line on the Rhombus
+    for(j=0;j<i;j++){
+      //Is the position even or odd so we change the color
+      if (j%2)
+        //even
+        lLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+      else
+        //odd
+        lLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+    }
+    lLine +="</p>";
+    // console.log(rLine);
+
+}
+
+document.getElementById("downLeft").innerHTML = lLine;
+}
+document.getElementById("downLeft").innerHTML = lLine;
 
 document.getElementById("downRight").innerHTML = rLine;
 }
