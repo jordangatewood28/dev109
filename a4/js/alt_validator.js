67d0170f89f26046491147bb70b62e74cfcf1668
jordangatewood28 <165770705+jordangatewood28@users.noreply.github.com>
2024-05-25 09:36:56 -0700

    Update alt_validator.js

--- a4/js/alt_validator.js
+++ a4/js/alt_validator.js
@@ -6,7 +6,7 @@ function validateForm() {
         document.getElementById("submiterror").innerHTML = "<p><strong>Error Submitting — See Above</strong></p>";
         event.preventDefault();
         return false;
-}
+};
 
 FirstName.addEventListener('blur', firstName, false);
 function firstName(){
