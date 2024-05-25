e038e35f74a7256303b8ef8875efdeb79118ae76
jordangatewood28 <165770705+jordangatewood28@users.noreply.github.com>
2024-05-25 07:34:01 -0700

    Update validator.js

--- a4/js/validator.js
+++ a4/js/validator.js
@@ -2,7 +2,9 @@ function validateForm() {
     if (firstName();
     lastName();
     email();
-    phone():
+    phone();
+    username();
+    password();
     )
     return true;
     else
@@ -23,13 +25,13 @@ function firstName(){
     //3) Do validation
     if (firstname==="null" || firstname==="" || firstname.length > 20 ) {
         FNerrorMessages += "<p>The first name is required and cannot be greater than 20 characters</p>";
-        console.log("First name invalid — length")
+        console.log("First name invalid — length");
         } else if (firstname.match("^[a-zA-Z ,.'-]+$")===null) {
             FNerrorMessages += "<p>Invalid caracter in last name (accepts only A-Z, a-z, and ,.'-)</p>";
             console.log("First name invalid — bad characters")
         } else {
                 var validFirstname = true;
-                console.log("First name valid")
+                console.log("First name valid");
         };
 
     //4) Send error message to HTML
@@ -51,13 +53,13 @@ function lastName(){
     //3) Do validation
     if (lastname==="null" || lastname==="" || lastname.length > 50 ) {
         LNerrorMessages += "<p>The last name is required and cannot be greater than 20 characters</p>";
-        console.log("Last name invalid — length")
+        console.log("Last name invalid — length");
         } else if (firstname.match("^[a-zA-Z ,.'-]+$")===null) {
             LNerrorMessages += "<p>Invalid caracter in last name (accepts only A-Z, a-z, and ,.'-)</p>";
-            console.log("Last name invalid — bad characters")
+            console.log("Last name invalid — bad characters");
         } else {
                 var validLastname = true;
-                console.log("Last name valid")
+                console.log("Last name valid");
         };
 
     //4) Send error message to HTML
@@ -68,43 +70,77 @@ function lastName(){
 };
 
 function email(){
-    var validEmail=false
-    
+    //1) Create variable
+    var validEmail=false;
+
+    //2) Read HTML value
     var userEmail = document.getElementById("email").value;
     var EmailerrorMessage = "";
-    
+
+    //3)Do validation
     var atpos = userEmail.indexOf("@");
     var dotpos = userEmail.lastIndexOf(".");
     if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length) {
-        EmailerrorMessage = "<p>You can only have one @ and period for your email</p>"
-        console.log("Email invalid.")
+        EmailerrorMessage = "<p>You can only have one @ and period for your email</p>";
+        console.log("Email invalid.");
     }
-    // send error message. For example:  errorMessages = "<p>Invalid email</p>";
     else{
-        validEmail = true
-        console.log("Email valid")
+        validEmail = true;
+        console.log("Email valid");
     }
 
+    //4) Send error message to HTML
     document.getElementById("email").innerHTML = EmailerrorMessages;
 
+    //5) Return status
     return (validEmail);
 };
 
 function phone(){
-    var validPhone = false
+    //1) Create variable
+    var validPhone = false;
 
+    //2) Read HTML value
     var userPhone = document.getElementByID("phone").value;
     var PhoneerrorMessage = "";
 
-    if (isNaN(phone) || phone.length > 15 || phone===null || phone ===""){
-        PhoneerrorMessage = "<p>Invalid phone number.</p>"
-        console.log("Phone invalid")
+    //3)Do validation
+    if (isNaN(phone) || phone.length > 15 || phone===null || phone === ""){
+        PhoneerrorMessage = "<p>Invalid phone number.</p>";
+        console.log("Phone invalid");
+        validPhone = false;
     }else{
-        validPhone = true
-        console.log("Phone valid")
+        validPhone = true;
+        console.log("Phone valid");
     };
 
+    //4) Send error message to HTML
     document.getElementById("phone").innerHTML = PhoneerrorMessage;
 
+    //5) Return status
     return (validPhone);
+}
+
+function password(){
+    //1) Create variable
+    var validPassword = false;
+
+    //2) Read HTML value
+    var userPassword = document.getElementByID("password").value;
+    var Passworderrormessage = "";
+
+    //3)Do validation
+    if (isNaN(password) || password.length > 7 || password === ""){
+        Passworderrormessage = "<p>Invalid password. Must be no more than 7 characters.</p>";
+        console.log("Password invalid");
+    }else{
+        validPassword = true;
+        console.log("Password valid");
+    }
+
+    //4) Send error message to HTML
+    document.getElementByID("password").innerHTML = PassworderrorMessage;
+
+    //5) Return status
+    return (validPassword);
 }
