d7b2bec84de1fee9f8017a0305bd0ac64c9e06b3
jordangatewood28 <165770705+jordangatewood28@users.noreply.github.com>
2024-06-10 16:16:38 -0700

    Create validator.js

---
+++ final/javascript/validator.js
@@ -0,0 +1,172 @@
+function isValid() {
+    if (firstName(),
+        lastName(),
+        email(),
+        phone(),
+        password(),
+        username()
+    )
+    return true;
+    else
+        document.getElementById("submiterror").innerHTML = "<p><strong>Error Submitting — See Above</strong></p>";
+        event.preventDefault();
+        return false;
+};
+
+FirstName.addEventListener('blur', firstName, false);
+function firstName(){
+    //1) Create variable
+    var validFirstname=false;
+
+    //2) read value from HTML
+    var firstname = document.getElementById("FirstName").value;
+    var errorMessages = "";
+
+    //3) Do validation
+    if (firstname==="null" || firstname==="" || firstname.length > 20 ) {
+        errorMessages += "<p>The first name is required and cannot be greater than 20 characters</p>";
+        console.log("First name invalid — length")
+        } else if (firstname.match("^[a-zA-Z ,.'-]+$")===null) {
+            errorMessages += "<p>Invalid caracter in last name (accepts only A-Z, a-z, and ,.'-)</p>";
+            console.log("First name invalid — bad characters")
+        } else {
+                validFirstname = true;
+                console.log("First name valid")
+        };
+
+    //4) Send error message to HTML
+    document.getElementById("fname").innerHTML = errorMessages;
+
+    //5) return status of each field
+    return (validFirstname);
+};
+
+function lastName(){
+    //1) Create variable
+    var validLastname=false;
+
+    //2) read value from HTML
+    var lastname = document.getElementById("LastName").value;
+    var LNerrorMessages = "";
+
+    //3) Do validation
+    if (lastname==="null" || lastname==="" || lastname.length > 50 ) {
+        LNerrorMessages += "<p>The last name is required and cannot be greater than 50 characters</p>";
+        console.log("Last name invalid — length");
+        } else if (firstname.match("^[a-zA-Z ,.'-]+$")===null) {
+            LNerrorMessages += "<p>Invalid caracter in last name (accepts only A-Z, a-z, and ,.'-)</p>";
+            console.log("Last name invalid — bad characters");
+        } else {
+                var validLastname = true;
+                console.log("Last name valid");
+        };
+
+    //4) Send error message to HTML
+    document.getElementById("lname").innerHTML = LNerrorMessages;
+
+    //5) return status of each field
+    return (validLastname);
+};
+
+Email.addEventListener('blur', userEmail, false);
+function email(){
+    //1) Create variable
+    var validEmail=false;
+
+    //2) Read HTML value
+    var userEmail = document.getElementById("Email").value;
+    var Emailerror = "";
+
+    //3)Do validation
+    var atpos = userEmail.indexOf("@");
+    var dotpos = userEmail.lastIndexOf(".");
+    if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length) {
+        EmailerrorMessage = "<p>You can only have one @ and period for your email</p>";
+        console.log("Email invalid.");
+    } else{
+        validEmail = true;
+        console.log("Email valid");
+    }
+
+    //4) Send error message to HTML
+    document.getElementById("email").innerHTML = Emailerror;
+
+    //5) Return status
+    return (validEmail);
+};
+
+Phone.addEventListener('blur', userPhone, false);
+function phone(){
+    //1) Create variable
+    var validPhone = false;
+
+    //2) Read HTML value
+    var userPhone = document.getElementByID("Phone").value;
+    var Phoneerror = "";
+
+    //3)Do validation
+    if (userPhone.length > 15 || userPhone===null || userPhone===""){
+        PhoneerrorMessage = "<p>A phone number is required and cannot be more than 15 characters.</p>";
+        console.log("Phone invalid");
+        validPhone = false;
+    } else{
+        validPhone = true;
+        console.log("Phone valid");
+    };
+
+    //4) Send error message to HTML
+    document.getElementById("phone").innerHTML = Phoneerror;
+
+    //5) Return status
+    return (validPhone);
+};
+
+Password.addEventListener('blur', userPassword, false);
+function password(){
+    //1) Create variable
+    var validPassword = false;
+
+    //2) Read HTML value
+    var userPassword = document.getElementByID("Password").value;
+    var Passworderror = "";
+
+    //3)Do validation
+    if (isNaN(userPassword) || userPassword.length > 7 || userPassword === ""){
+        Passworderrormessage = "<p>Invalid password. Must be no more than 7 characters.</p>";
+        console.log("Password invalid");
+    } else{
+        validPassword = true;
+        console.log("Password valid");
+    }
+
+    //4) Send error message to HTML
+    document.getElementByID("password").innerHTML = Passworderror;
+
+    //5) Return status
+    return (validPassword);
+};
+
+Username.addEventListener('blur', Username, false);
+function username(){
+    //1) Create variable
+    var validUsername = false;
+
+    //2)Read HTML value
+    var Username = document.getElementByID("Username").value;
+    var Usernameerror = ""
+
+    //3) Do validation
+    if (Username==="null" || Username.length > 12 || Username === ""){
+        Usernameerror = "<p>A username is required and must be no more than 12 characters.</p>";
+        console.log("Username invalid");
+    } else{
+        validUsername = true
+        console.log("Username valid");
+    }
+
+    //4) Send error message to HTML
+    document.getElementByID("username").innerHTML = Usernameerror;
+
+    //5) Return status
+    return (validUsername);
+};
