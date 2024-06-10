c42de346e9ab8413da03d7d473221deff2483b12
jordangatewood28 <165770705+jordangatewood28@users.noreply.github.com>
2024-06-10 16:51:42 -0700

    Create slideshow.js

---
+++ final/javascript/slideshow.js
@@ -0,0 +1,51 @@
+var myImages =[];
+
+
+var captionImages =["HULL DOWN: ","TURRET DOWN:","SMOKE SCREEN:"];
+
+ var index=0; 
+
+ function updateImage(){
+ document.getElementById("slideshow").src = myImages[index];
+ document.getElementById("slideshow").alt= captionImages[index];
+ document.getElementById("caption").textContent = captionImages[index]; 
+} 
+
+function next(){
+ if (myImages.length == index+1)
+ index=0;
+ else
+ index++;
+ updateImage();
+} 
+ 
+
+function back(){
+ if (index===0)
+ index=myImages.length-1;
+ else
+ index--;
+ 
+ updateImage();
+} 
+
+function autoSlide(){
+if (document.getElementById("auto").checked)
+ next(); 
+}
+
+setInterval(autoSlide,2000); // Next
+
+var nextButton = document.getElementById("next"); 
+var previousButton = document.getElementById("previous"); 
+
+previousButton.addEventListener("click",back,false);
+nextButton.addEventListener("click",next,false);
+
+function autoSlide(){
+if (document.getElementById("auto").checked)
+ next(); 
+}
+
+
+setInterval(autoSlide,2000); // Next
